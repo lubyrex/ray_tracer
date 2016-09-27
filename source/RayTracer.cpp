5dc4a001fd9fdd246d06ed8a186ff2fda220b14d
mc13 <mc13@61224a88-a156-e611-bf22-b8ac6f268050>
2016-09-27 02:02:09 +0000

    Light graphing code taken out

--- source/RayTracer.cpp
+++ source/RayTracer.cpp
@@ -185,8 +185,8 @@ Radiance3 RayTracer::L_o(const shared_ptr<Surfel>& surfelX, const Vector3& wo, c
     const Point3& X(surfelX->position);
     const Vector3& n(surfelX->shadingNormal);
 
-    Thread::runConcurrently(Point2int32(0, 0), Point2int32(/*lights.size()*/20, 1), [&](Point2int32 i) {
-        const shared_ptr<Light>& light(lights[1]);
+    Thread::runConcurrently(Point2int32(0, 0), Point2int32(lights.size(), 1), [&](Point2int32 i) {
+        const shared_ptr<Light>& light(lights[i.x]);
         const Point3& Y(light->position().xyz());
 
         if (isVisible(X, Y, surfelX->geometricNormal.direction())) {
